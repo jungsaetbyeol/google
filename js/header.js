721e3b4c3b42d36cbcf94aba4d960e5d0e0bdbb0
jung saetbyeol <byeol12284@gmail.com>
2025-01-21 13:12:45 +0900

    해더 완성
    제이쿼리 반응형 메뉴 나타내기

--- js/header.js
+++ js/header.js
@@ -8,6 +8,13 @@ $(document).ready(function () {
       300
     );
   });
+  // 모바일용 메뉴 클릭시
+  $(".mobile-nav .gnb > li > a").click(function () {
+    // console.log(this);
+
+    $(this).next(".mobile-nav .depth2").stop().slideToggle();
+  });
+
   //   모바일 메뉴 닫기 버튼
   $("#close-menu").click(function () {
     $(".mobile-nav").animate(
@@ -17,4 +24,18 @@ $(document).ready(function () {
       300
     );
   });
+  // 메뉴에 호버했을때
+  $("nav .gnb").hover(
+    function () {
+      // 마우스를 올렸을때
+      $("nav .depth2 , .header-bg").stop().slideDown();
+    },
+    function () {
+      // 마우스를 땟을때
+      $("nav .depth2 , .header-bg").stop().slideUp();
+    }
+  );
+  
+  
 });
+
